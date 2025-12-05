dfa829b79a2c733c67d1350988f82d591fa76301
Alamgir Sarder <alamgirsarderinfo@gmail.com>
2025-12-05 16:23:27 +0600

    about part runnning

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -6,6 +6,17 @@ import about_laptop from "../../assets/about_laptop.png";
 import badge from "../../assets/badge.png";
 
 const About = () => {
+  const my_details = 
+    {
+      name: "Alamgir Sarder",
+      email: "alamgirsarderinfo@gmailcom",
+      phone: "+88 01642-252919",
+      address: "Dhaka",
+      degree: "Dimploma",
+      freelancer: "Available",
+    }
+ 
+
   return (
     <div className="bg-basecolor">
       <Container>
@@ -21,21 +32,23 @@ const About = () => {
             <Flex className="justify-between">
               <div>
                 <Flex className="relative w-[667px]">
-                <img
-                  className="w-[95px] h-[95px] mt-[151px] mr-[108px]"
-                  src={badge}
-                  alt="bagde"
-                />
-                <img src={about_man} alt="about manp" />
-                <img
-                  className="absolute top-0 left-0 mt-[386px]"
-                  src={about_laptop}
-                  alt="about laptop"
-                />
-              </Flex>
+                  <img
+                    className="w-[95px] h-[95px] mt-[151px] mr-[108px] animate-bounce"
+                    src={badge}
+                    alt="bagde"
+                  />
+                  <img src={about_man} alt="about manp" />
+                  <img
+                    className="absolute top-0 left-0 mt-[386px]"
+                    src={about_laptop}
+                    alt="about laptop"
+                  />
+                </Flex>
               </div>
               <div className="w-[607px]">
-                <h2 className="font-primary font-semibold text-white text-5xl mb-8">About Me</h2>
+                <h2 className="font-primary font-semibold text-white text-5xl mb-8">
+                  About Me
+                </h2>
                 <p className="font-primary font-normal text-lg text-white leading-[123%] w-[597px] text-justify">
                   Duis aute irure dolor in reprehenderit in voluptate velit esse
                   cillum dolore eu fugiat nulla pariatur. Excepteur sint occae
@@ -43,6 +56,25 @@ const About = () => {
                   runt mollit anim id est laborum velit esse cillum dolore eu
                   fugiat.
                 </p>
+
+                  <Flex className="mt-14">
+                    <div className="pt-8 pl-10 pb-10 w-[304px] border border-[#00C0FF] rounded-tl-[14px] rounded-bl-[14px]">
+                       <h3 className="font-primary font-semibold text-2xl text-white">Name</h3>
+                    <h2 className="font-medium font-primary text-xl text-[#B4AFC6] mt-1.5">{my_details?.name}</h2>
+                    <h3 className="font-primary font-semibold text-2xl text-white mt-5">Email</h3>
+                    <h2 className="font-medium font-primary text-lg text-[#B4AFC6] mt-1.5">{my_details?.email}</h2>
+                     <h3 className="font-primary font-semibold text-2xl text-white mt-5">Phone</h3>
+                    <h2 className="font-medium font-primary text-xl text-[#B4AFC6] mt-1.5">{my_details?.phone}</h2>
+                    </div>
+                    <div className="pt-8 pl-10 pb-10 w-[304px] border border-[#00C0FF] rounded-tr-[14px] rounded-br-[14px] border-l-0">
+                       <h3 className="font-primary font-semibold text-2xl text-white">Address</h3>
+                    <h2 className="font-medium font-primary text-xl text-[#B4AFC6] mt-1.5">{my_details?.address}</h2>
+                    <h3 className="font-primary font-semibold text-2xl text-white mt-5">Degree</h3>
+                    <h2 className="font-medium font-primary text-xl text-[#B4AFC6] mt-1.5">{my_details?.degree}</h2>
+                     <h3 className="font-primary font-semibold text-2xl text-white mt-5">Freelancer</h3>
+                    <h2 className="font-medium font-primary text-xl text-[#B4AFC6] mt-1.5">{my_details?.freelancer}</h2>
+                    </div>
+                  </Flex>
               </div>
             </Flex>
           </div>
